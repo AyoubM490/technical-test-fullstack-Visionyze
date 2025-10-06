2316a73fd024cc0767de389abfd533f29105da35
Ayoub Chahir <ayoub.chahir+kioskapp@kiosk360.ca>
2025-10-06 09:14:03 -0400

    implémenter le webhook stripe

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import { stripe } from "@lib/stripe";
+import { headers } from "next/headers";
+import { prisma } from "@lib/prisma";
 
 /**
  * TODO:
@@ -10,6 +14,40 @@ export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
 export async function POST(req: NextRequest) {
-  // TODO: implémenter la validation + persistance
-  return NextResponse.json({ error: "NOT_IMPLEMENTED" }, { status: 501 });
+  const body = await req.text();
+  const signature = (await headers()).get('Stripe-Signature') as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  } catch (error) {
+    return new NextResponse("invalid signature", { status: 400 });
+  }
+  
+  const session = event.data.object as Stripe.Checkout.Session;
+  
+  if (event.type === 'checkout.session.completed') {
+
+    // ✅ Écriture en base = "source de vérité"
+    await prisma.payment.upsert({
+        where: { stripeSessionId: session.id },
+        update: {
+          status: session.payment_status,
+        },
+        create: {
+          stripeSessionId: session.id,
+          amount: session.amount_total ?? 0,
+          currency: session.currency ?? "eur",
+          status: session.payment_status,
+          customerEmail: session.customer_details?.email ?? null,
+        },
+      });
+  }
+
+  return new NextResponse("ok", { status: 200 });
 }
